554c04e664959e79925b90b042a3666ecb489a3a
Ali Mohammad Tabatabaei <a.m.tabatabaei123@gmail.com>
2022-12-28 19:37:04 +0330

    fixed piping issues

--- shell.c
+++ shell.c
@@ -125,12 +125,40 @@ void execArgsPiped(char** parsed, char** parsedpipe)
 			}
 		} else {
 			// parent executing, waiting for two children
-			wait(NULL);
-			wait(NULL);
+			//printf("\nExecuting command");
+			//wait(NULL);
+			//wait(NULL);
 		}
 	}
 }
 
+void cisshPipe(char **command1, char **command2)
+{
+    int fd[2];
+    pid_t childPid;
+    if (pipe(fd) != 0)
+        error("failed to create pipe");
+
+    if ((childPid = fork()) == -1)
+        error("failed to fork");
+
+    if (childPid == 0)
+    {
+        dup2(fd[1], 1);
+        close(fd[0]);
+        close(fd[1]);
+        execvp(command1[0], command1);
+        error("failed to exec command 1");
+    }
+    else
+    {
+        dup2(fd[0], 0);
+        close(fd[0]);
+        close(fd[1]);
+        execvp(command2[0], command2);
+        error("failed to exec command 2");
+    }
+}
 // Help command builtin
 void openHelp()
 {
